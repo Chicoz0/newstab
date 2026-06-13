bf2afa5f86f7d88ba6da13d1b23974b8705a1a9b
chicoz0 <franciscolemosfisio@gmail.com>
2026-06-13 11:37:58 -0300

    feat: add status endpoint + teste + remover coisas relacionadas a calculadora

--- tests/calculadora.test.js
+++
@@ -1,9 +0,0 @@
-const calculadora = require("../models/calculadora");
-
-test("espero que 1 seja 1", () => {
-  expect(calculadora.somar(1, 1)).toBe(2);
-});
-
-test("espero que 2*2 seja 4", () => {
-  expect(calculadora.multiplicar(2, 2)).toBe(4);
-});
